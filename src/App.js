50f9bd47f06328914372cfcb3e18bb834ccfe2fb
BenRiVarg <benrivarr@outlook.com>
2022-05-20 00:44:35 -0500

    Cambio de trifuerza ahora sí

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
+import logo from './img/trifuerza2.png';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
